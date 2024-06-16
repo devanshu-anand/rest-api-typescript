7b043727d57d23629a6162507b1674a54e2f529a
devanshu <itsdevanshu@hotmail.com>
2024-06-16 17:40:41 +0530

    added changes till login

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import router from './router';
 
 const app = express();
 
@@ -25,7 +26,13 @@ server.listen(PORT,()=>{
 })
 
 
-const MONGO_URL = "mongodb://localhost:27017/";
+const MONGO_URL = "mongodb://127.0.0.1:27017/";
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on('error', (error: Error) => console.log(error));
+
+app.use('/',router())
+
+app.use('*',() => {
+    console.log("page not found !!")
+})
